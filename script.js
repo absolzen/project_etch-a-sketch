b59e738a2c12de9d0c768aaaa116a3a9dd1a6129
absolzen <robdomu@gmail.com>
2024-07-12 18:51:35 +0200

    Create grid

--- script.js
+++ script.js
@@ -3,6 +3,5 @@ const grid = document.querySelector('.grid');
 // create 256 divs
 for (let i = 0; i < 256; i++) {
   const gridElement = document.createElement('div');
-  gridElement.textContent = '+';
   grid.appendChild(gridElement);
 }
