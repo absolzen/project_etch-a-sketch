7cee3ac107d96c8a26f15867e90baf1827251178
absolzen <robdomu@gmail.com>
2024-07-12 19:01:19 +0200

    Add comments with TODO'S

--- script.js
+++ script.js
@@ -5,3 +5,14 @@ for (let i = 0; i < 256; i++) {
   const gridElement = document.createElement('div');
   grid.appendChild(gridElement);
 }
+
+// TODO: 
+// - add background color change for grid elements on mouse hover
+// - add button on top asking for a number of grid elements per side for the new grid
+//   - remove existing grid
+//   - create new grid within the same space as before
+//   - limit user input to max 100
+
+// EXTRA:
+// - add randomization of square colors
+// - add progressive darkening (each step darkens color by 10%). Use opacity.
