0e3fc3d9e7753a3205905993270f0d77deb84456
absolzen <robdomu@gmail.com>
2024-07-13 13:02:12 +0200

    Add square size calculation

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 const grid = document.querySelector('.grid');
 const gridSizeBtn = document.querySelector('.gridSize');
 
-let side = 0;
+let side = 1;
 
 function calculateGrid(side) {
   return gridSize = side * side;
 }
 
 function drawGrid(size) {
-  // create grid with
+  // create grid with specified size
   for (let i = 0; i < gridSize; i++) {
     const gridElement = document.createElement('div');
     grid.appendChild(gridElement);
@@ -19,18 +19,25 @@ function drawGrid(size) {
 let gridStyle = window.getComputedStyle(grid),
     gridWidth = +gridStyle.getPropertyValue('width').replace('px', '');
 
-
-
+    
+    
+    
+// get user input for number of squares and draw a grid based on that
 gridSizeBtn.addEventListener('click', () => {
   side = +prompt('Enter grid side size: ', 16);
   drawGrid(calculateGrid(side));
 });
-
+    
 // select all grid elements and create array from them
 const gridElements = Array.from(document.querySelectorAll('.grid > div'));
+    
+// 960 / 16 == 60 // gridWidth / side == gridElement
+// set square width and height to that
+let gridElementWidth = gridWidth / side;
 
 // attach event listener for each square and change its background on mouse hover
 gridElements.forEach((element) => {
+  element.setAttribute(`width: ${gridElementWidth}`); 
   element.addEventListener('mouseover', () => {
     element.style.backgroundColor = 'black';
   });
